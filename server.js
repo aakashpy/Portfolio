2f659bae6fcfede39ea5b5642bf1a2d7d6e1ad06
Aakash <gholeaakash03@gmail.com>
2021-10-29 10:32:23 +0530

    deploying project

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
+    app.use(express.static(__dirname + '/dist/client/build'))
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+        res.sendFile(path.resolve(__dirname, 'dist','client','build','index.html'));
     })
 }
 
