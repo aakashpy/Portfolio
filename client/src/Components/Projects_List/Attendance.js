9d14c194839ed952360c827f078fb2cc4fd9b9c7
Aakash <gholeaakash03@gmail.com>
2021-10-31 22:07:02 +0530

    minor changes

--- client/src/Components/Projects_List/Attendance.js
+++ client/src/Components/Projects_List/Attendance.js
@@ -270,15 +270,15 @@ const Attendance = (props) => {
             </div>
             <section className="text-white body-font">
                 <div className="px-5 py-12 mx-auto">
-                    <div className="flex flex-wrap -m-2 justify-center">
-                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4 bg-gray-500 rounded-lg mr-2 ml-1">
+                    <div className="flex flex-wrap -m-2 justify-center md:h-96">
+                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4  bg-gray-500 rounded-lg mr-2 ml-1 relative">
                             <div className="h-full text-center">
                                 <img alt="Aakash" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={require('../Images/CONTRIBUTORS/Aakash.jpg').default}/>
-                                    <p className="text-xl">Aakash Ghole</p>
+                                    <p className="text-3xl">Aakash Ghole</p>
                                     <p className="leading-relaxed">3rd Year Computer Science Student at Atharva College of Engineering, Mumbai Unversity</p>
                                     
                                     <p className="text-white mb-3 mt-3">Full stack Web Developer,GUI Developer and Android Developer</p>
-                                    <div className="flex justify-center">
+                                    <div className="flex justify-center md:absolute md:bottom-1 md:inset-x-0">
                                     <a href="https://github.com/aakashpy" target="_blank" rel="noreferrer"className="text-white">
                                         <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
                                         src={require('../Images/github.png').default} alt="github"/>
@@ -294,14 +294,14 @@ const Attendance = (props) => {
                                     </div>
                             </div>
                         </div>
-                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4 bg-gray-500 rounded-lg mr-1 ml-1">
+                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4 bg-gray-500 rounded-lg mr-1 ml-1 relative">
                             <div className="h-full text-center">
                                 <img alt="Sneha" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={require('../Images/CONTRIBUTORS/Sneha.jpg').default}/>
-                                <p className="text-xl">Sneha Dhumal</p>
+                                <p className="text-3xl">Sneha Dhumal</p>
                                     <p className="leading-relaxed">3rd Year Computer Science Student at Atharva College of Engineering, Mumbai Unversity</p>
                                     
                                     <p className="text-white mb-3 mt-3">Full stack Web Developer and UI designer</p>
-                                    <div className="flex justify-center">
+                                    <div className="flex justify-center md:absolute md:bottom-1 md:inset-x-0">
                                     <a href="https://github.com/SnehaDhumal1508" target="_blank" rel="noreferrer"className="text-white">
                                         <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
                                         src={require('../Images/github.png').default} alt="github"/>
