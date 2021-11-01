9921c82be4a6811f921573a5a4e26ff490385981
Aakash <gholeaakash03@gmail.com>
2021-11-01 18:57:46 +0530

    added all contributors

--- client/src/Components/Projects_List/Attendance.js
+++ client/src/Components/Projects_List/Attendance.js
@@ -317,14 +317,14 @@ const Attendance = (props) => {
                                     </div>
                             </div>
                         </div>
-                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4 bg-gray-500 rounded-lg mr-1 ml-2">
+                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4 bg-gray-500 rounded-lg mr-1 ml-2 relative">
                             <div className="h-full text-center">
                                 <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={require('../Images/CONTRIBUTORS/Dhairya.jpg').default}/>
                                 <p className="text-xl">Dhairya Honrao</p>
                                     <p className="leading-relaxed">3rd Year Computer Science Student at Atharva College of Engineering, Mumbai Unversity</p>
                                     
                                     <p className="text-white mb-3 mt-3">IOT specialist and UI designer</p>
-                                    <div className="flex justify-center">
+                                    <div className="flex justify-center md:absolute md:bottom-1 md:inset-x-0">
                                     <a href="https://github.com/DhairyaHonrao" target="_blank" rel="noreferrer"className="text-white">
                                         <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
                                         src={require('../Images/github.png').default} alt="github"/>
