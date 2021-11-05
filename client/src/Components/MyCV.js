1e3f77a3af7dd891356417cf8fa83ada2c07f140
Aakash <gholeaakash03@gmail.com>
2021-11-05 12:14:13 +0530

    df

--- client/src/Components/MyCV.js
+++ client/src/Components/MyCV.js
@@ -4,23 +4,35 @@ import axios from 'axios'
 
 const MyCV = (props) => {
     //const names = ['Bruce', 'Clark', 'Diana']
-    // const [email,setemail]=useState([])
-    // useEffect(()=>{
-    //     axios.get(`/api/email/getemails`)
-    //     .then(res => {
-    //        setemail(res.data)
-    //     })
-    //     .catch(err=>{
-    //         console.log(err)
-    //     }) 
-    // },[]) 
-    const email=[
-        {
-            _id:121,
-            email:"asd"
-        },
-    ]
-    console.log(email)           
+    
+    const [email,setemail]=useState([])
+    useEffect(()=>{
+        axios.get(`/api/email/getemails`)
+        .then(res => {return (res.data)})
+        .then((data)=>{
+            if(!data.error){
+                console.log("yaaay no error")
+                setemail(data)
+                return data
+                
+            }else{
+                console.log("error") 
+            }
+        })
+        .then(()=>console.log(email))
+        .catch(err=>{
+            console.log(err)
+            console.log("bruh")
+        }) 
+    },[])
+    //setting_email=email 
+    // const email=[
+    //     {
+    //         _id:121,
+    //         email:"asd"
+    //     },
+    // ]
+               
     return (
         <div className="text-white bg-gradient-to-r from-blue-900 via-black to-blue-900 h-screen">
         {props.navbar}
