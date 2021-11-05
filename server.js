3ebb4488e7aaf0151fbf37351d793693b3a2734d
Aakash <gholeaakash03@gmail.com>
2021-11-05 19:19:47 +0530

    server

--- server.js
+++ server.js
@@ -17,14 +17,13 @@ mongoose.connect(uri, {
 app.use(cors());
 app.use(express.json());
 const Email = require("./api/email")
+app.use("/api/email", Email);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '/client/build/index.html'));
     })
-    app.use("/api/email", Email);
 }
-
 const port = process.env.PORT || 5000;
-app.use("/api/email", Email);
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
