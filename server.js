2bdec274e39722fcf53e5c92722fdd3d99cdf5dc
Aakash <gholeaakash03@gmail.com>
2021-11-05 19:15:36 +0530

    server

--- server.js
+++ server.js
@@ -4,28 +4,27 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path')
 const app = express()
-app.use(cors());
-app.use(express.json());
+
 
 const uri = String(process.env.ATLAS_URI);
-mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology: true,
+mongoose.connect(uri, {
+    useNewUrlParser: true, useUnifiedTopology: true,
 })
-.then(() => console.log("connection extablished!"))
-.catch((err) => {
-    console.log(err);
-});
+    .then(() => console.log("connection extablished!"))
+    .catch((err) => {
+        console.log(err);
+    });
+app.use(cors());
+app.use(express.json());
 const Email = require("./api/email")
-if(process.env.NODE_ENV==='production'){
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
-    app.get('*',(req,res)=>{
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '/client/build/index.html'));
     })
-    app.use("/api/email",Email);
+    app.use("/api/email", Email);
 }
 
 const port = process.env.PORT || 5000;
-app.get("/api/", (request, response) => {
-    response.json({ message: "Server is working!!!!!!" });
-});
-app.use("/api/email",Email);
-app.listen(port,()=>console.log(`Server started on port ${port}`));
+app.use("/api/email", Email);
+app.listen(port, () => console.log(`Server started on port ${port}`));
