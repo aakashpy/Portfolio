42874cf23a50a8afe3eacf83ced864935dcfa4a7
Aakash <gholeaakash03@gmail.com>
2021-11-06 19:07:12 +0530

    Contact Page Completed

--- client/src/Components/ContactMe.js
+++ client/src/Components/ContactMe.js
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Modal from "@material-tailwind/react/Modal";
 import ModalBody from "@material-tailwind/react/ModalBody";
 import ModalFooter from "@material-tailwind/react/ModalFooter";
 import Button from "@material-tailwind/react/Button";
 import validator from 'validator'
+import emailjs from 'emailjs-com'
 import axios from 'axios'
 import insta from './Images/insta.png'
 import linkdin from './Images/linkdin.png'
+require('dotenv').config()
 
 const ContactMe = (props) => {
-
     //for subscribe
     const [showModal, setShowModal] = useState(false);
     const [email_name, setemailName] = useState('');
@@ -105,20 +106,18 @@ const ContactMe = (props) => {
     }
 
     //for submit
-    const [form_modal,setform_modal] = useState(true);
-    const [form_submitted,setform_submitted] = useState(false);
+    const form = useRef();
+    const [form_modal, setform_modal] = useState(true);
+    const [form_submitted, setform_submitted] = useState(false);
     const [values, setValues] = useState({});
     const handleChange = (e) => {
         const { name, value } = e.target;
         setValues(values => ({ ...values, [name]: value, }));
     }
     const onSubmit = (e) => {
-        // console.log(values.name)
-        // console.log(values.email)
-        // console.log(values.message)
         e.preventDefault();
-        if (values.email!=='' && validator.isEmail(values.email)) {
-            if ( values.name !== "" && values.name !== "") {
+        if (values.email !== '' && validator.isEmail(values.email)) {
+            if (values.name !== "" && values.name !== "") {
                 setform_modal(false)
                 axios.get(`/api/email/${values.email}`)
                     .then(res => {
@@ -130,12 +129,17 @@ const ContactMe = (props) => {
                         }
                         else {
                             // submit there response
-                            console.log("your response is submitted")
-                            setShowModal(true)
-                            setform_submitted(true)
+                            emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, form.current, process.env.REACT_APP_EMAILJS_USER_ID)
+                                .then((result) => {
+                                    console.log(result.text);
+                                    setShowModal(true)
+                                    setform_submitted(true)
+                                }, (error) => {
+                                    console.log(error.text);
+                                });
                         }
                     }
-                )
+                    )
             }
         }
     }
@@ -151,36 +155,36 @@ const ContactMe = (props) => {
                             <p className="lg:w-2/3 mx-auto leading-relaxed text-2xl">I will get in touch with you soon...</p>
                         </div>
                         <div className="lg:w-1/2 md:w-2/3 mx-auto">
-                            <form onSubmit={onSubmit}>
-                            <div className="flex flex-wrap -m-2">
-                                <div className="p-2 w-1/2">
-                                    <div className="relative">
-                                        <label htmlFor="name" className="leading-7 text-md text-white">Name</label>
-                                        <input type="text" id="name" name="name" value={values.name || ''} onChange={handleChange} required
-                                            className="w-full rounded border border-gray-300 focus:border-indigo-500 bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            <form onSubmit={onSubmit} ref={form}>
+                                <div className="flex flex-wrap -m-2">
+                                    <div className="p-2 w-1/2">
+                                        <div className="relative">
+                                            <label htmlFor="name" className="leading-7 text-md text-white">Name</label>
+                                            <input type="text" id="name" name="name" value={values.name || ''} onChange={handleChange} required
+                                                className="w-full rounded border border-gray-300 focus:border-indigo-500 bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="p-2 w-1/2">
-                                    <div className="relative">
-                                        <label htmlFor="email" className="leading-7 text-md text-white">Email</label>
-                                        <input type="email" id="email" name="email" value={values.email || ''} onChange={handleChange} required
-                                            className="w-full rounded border border-gray-300 focus:border-indigo-500 bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <div className="p-2 w-1/2">
+                                        <div className="relative">
+                                            <label htmlFor="email" className="leading-7 text-md text-white">Email</label>
+                                            <input type="email" id="email" name="email" value={values.email || ''} onChange={handleChange} required
+                                                className="w-full rounded border border-gray-300 focus:border-indigo-500 bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="p-2 w-full">
-                                    <div className="relative">
-                                        <label htmlFor="message" className="leading-7 text-md text-white">Message</label>
-                                        <textarea id="message" name="message" value={values.message || ''} onChange={handleChange} required
-                                            className="w-full rounded border border-gray-300 focus:border-indigo-500 bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                    <div className="p-2 w-full">
+                                        <div className="relative">
+                                            <label htmlFor="message" className="leading-7 text-md text-white">Message</label>
+                                            <textarea id="message" name="message" value={values.message || ''} onChange={handleChange} required
+                                                className="w-full rounded border border-gray-300 focus:border-indigo-500 bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="p-2 w-full">
-                                    <button className="text-white rounded-lg md:h-auto md:w-28 border-2 border-double text-center text-xl
+                                    <div className="p-2 w-full">
+                                        <button className="text-white rounded-lg md:h-auto md:w-28 border-2 border-double text-center text-xl
                                     flex justify-center m-auto px-5 py-3 bg-gradient-to-r from-blue-500 via-black to-blue-500
                                     hover:from-black hover:via-blue-500 hover:to-black focus:from-black focus:via-blue-500 focus:to-black"
-                                    type="submit">Submit</button>
+                                            type="submit">Submit</button>
+                                    </div>
                                 </div>
-                            </div>
                             </form>
                         </div>
                         <div className="text-white text-2xl text-center mt-6 md:mt-12">Follow me on other social media</div>
@@ -202,45 +206,44 @@ const ContactMe = (props) => {
                                     hover:from-black hover:via-blue-500 hover:to-black focus:from-black focus:via-blue-500 focus:to-black"
                                     onClick={(e) => setShowModal(true)}>Subscribe</button>
                             </div>
-                            <div>to get all notifications via email</div>
+                            <div>to get all notifications via email and would be sent via your email</div>
                         </div>
                     </div>
                 </section>
             </div>
             <Modal size="lg" active={showModal} toggler={() => setShowModal(false)}>
-            {form_modal ? <ModalBody>
+                {form_modal ? <ModalBody>
                     <>
-                    {isSubscribed ? <div className="text-center">You are now subscribed</div> :
-                        <div className="p-2 w-full text-center">
-                            <div className="text-center pb-2">
-                                <label htmlFor="email" className="leading-7 text-xl text-black ">Enter your email id</label>
-                                <input type="email" id="email" name="email" onChange={handleChangeSubscribe} value={email_name} required
-                                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                            </div>
-                            {(otp_flag === '1') &&
+                        {isSubscribed ? <div className="text-center">You are now subscribed</div> :
+                            <div className="p-2 w-full text-center">
                                 <div className="text-center pb-2">
-                                    <label htmlFor="opt" className="leading-7 text-xl text-black ">Enter otp sent to your email id</label>
-                                    <input type="number" id="otp" name="otp" onChange={otp_number} value={otp}
+                                    <label htmlFor="email" className="leading-7 text-xl text-black ">Enter your email id</label>
+                                    <input type="email" id="email" name="email" onChange={handleChangeSubscribe} value={email_name} required
                                         className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
-                            }
-                            {email_flag === '1' ? <div className="text-xl">You have already subscribed :)</div> : <div className="text-xl">{emailError}</div>}
-                        </div>}
+                                {(otp_flag === '1') &&
+                                    <div className="text-center pb-2">
+                                        <label htmlFor="opt" className="leading-7 text-xl text-black ">Enter otp sent to your email id</label>
+                                        <input type="number" id="otp" name="otp" onChange={otp_number} value={otp}
+                                            className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    </div>
+                                }
+                                {email_flag === '1' ? <div className="text-xl">You have already subscribed :{')'}</div> : <div className="text-xl">{emailError}</div>}
+                            </div>}
                     </>
                 </ModalBody>
-                :<ModalBody>
-                    <>
-                    {form_submitted ? <div className="p-2 w-full text-center">
-                    your response is submitted
-                </div>
-                :
-                <div className="p-2 w-full text-center">
-                    you have to subcribe first for security reasons
-                </div>}
-                
-                </>
-                </ModalBody>}
-                
+                    : <ModalBody>
+                        <>
+                            {form_submitted ? <div className="p-2 w-full text-center text-xl text-green-700 font-semibold">
+                                Your response is submitted
+                            </div>
+                                :
+                                <div className="p-2 w-full text-center text-xl text-red-700 font-semibold">
+                                    You have to subcribe first for security reasons
+                                </div>}
+                        </>
+                    </ModalBody>}
+
                 <ModalFooter>
                     <div className="mx-auto">
                         {isValid ? <Button
@@ -258,10 +261,10 @@ const ContactMe = (props) => {
                             {(otp_flag_button !== '1' && otp_flag_button !== '2') && 'Check email'}
                             {otp_flag_button === '2' && 'Subscribe'}
                             {otp_flag_button === '1' && 'Close'}
-                        </Button>:
-                        <Button onClick={()=>setShowModal(false)}>
-                        Close
-                    </Button>}
+                        </Button> :
+                            <Button onClick={() => setShowModal(false)}>
+                                Close
+                            </Button>}
                     </div>
                 </ModalFooter>
             </Modal>
