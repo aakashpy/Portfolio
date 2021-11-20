4ac59830bb889f0ef893f2ddd4e5a03843ebe3a2
Aakash <gholeaakash03@gmail.com>
2021-11-20 19:45:10 +0530

    minor changes

--- client/src/Components/Projects_List/Attendance.js
+++ client/src/Components/Projects_List/Attendance.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import NodeMCU from '../Images/ATTENDANCE/NodeMCU.jpg'
-import fingerprint from '../Images/ATTENDANCE/fingerprint.jpg'
-import pushbuttons from '../Images/ATTENDANCE/pushbuttons.jpg'
-import i2c_lcd from '../Images/ATTENDANCE/i2c_lcd.jpg'
-import jumpers from '../Images/ATTENDANCE/jumpers.jpg'
-import breadboard from '../Images/ATTENDANCE/breadboard.jpg'
+import Contributors from '../../Static_Component/Contributors'
 
 const Attendance = (props) => {
     return (
@@ -32,7 +27,7 @@ const Attendance = (props) => {
                             <div className="p-4 ml-2 mr-2 mb-3 border-2 rounded-lg">
                                 <div className=" border-2 rounded-lg p-3 bg-gray-800 md:h-100 relative">
                                     <div className="block  h-48 rounded">
-                                        <img alt="NodeMCU" className="object-center w-full h-full block p-2 rounded-lg" src={NodeMCU} />
+                                        <img alt="NodeMCU" className="object-center w-full h-full block p-2 rounded-lg" src={require('../Images/ATTENDANCE/NodeMCU.jpg').default} />
                                     </div>
                                     <h3 className="text-white text-2xl font-semibold mb-1 mt-3">NodeMCU</h3>
                                     <h4 className="text-white text-md font-medium pb-4">NodeMCU is a low-cost open source IoT platform. It initially included firmware which runs on the ESP8266 Wi-Fi SoC from Espressif Systems, and hardware which was based on the ESP-12 module. Later, support for the ESP32 32-bit MCU was added.</h4>
@@ -46,7 +41,7 @@ const Attendance = (props) => {
                             <div className="p-4 ml-2 mr-2 mb-3 border-2 rounded-lg">
                                 <div className="border-2 rounded-lg p-3 bg-gray-800 md:h-100 relative">
                                     <div className="block relative h-48 rounded ">
-                                        <img alt="fingerprint" className="object-center w-full h-full block p-2 rounded-lg" src={fingerprint} />
+                                        <img alt="fingerprint" className="object-center w-full h-full block p-2 rounded-lg" src={require('../Images/ATTENDANCE/fingerprint.jpg').default} />
                                     </div>
                                     <h3 className="text-white text-2xl font-semibold mb-1 mt-3">Adafruit Finger Print Sensor</h3>
                                     <h4 className="text-white text-md font-medium pb-4">Secure your project with biometrics - this all-in-one optical fingerprint sensor will make adding fingerprint detection and verification super simple. These modules are typically used in safes - there's a high powered DSP chip that does the image rendering, calculation, feature-finding and searching.</h4>
@@ -59,7 +54,7 @@ const Attendance = (props) => {
                             <div className="p-4 ml-2 mr-2 mb-3 border-2 rounded-lg">
                                 <div className="border-2 rounded-lg p-3 bg-gray-800 md:h-100 relative">
                                     <div className="block relative h-48 rounded">
-                                        <img alt="pushbuttons" className="object-center w-full h-full block p-2 rounded-lg" src={pushbuttons} />
+                                        <img alt="pushbuttons" className="object-center w-full h-full block p-2 rounded-lg" src={require('../Images/ATTENDANCE/pushbuttons.jpg').default} />
                                     </div>
                                     <h3 className="text-white text-2xl font-semibold mb-1 mt-3">Push Buttons</h3>
                                     <h4 className="text-white text-lg font-medium pb-4">A push-button is a simple switch mechanism to control some aspect of a machine or a process. Buttons are typically made out of hard material, usually plastic or metal. The surface is usually flat or shaped to accommodate the human finger or hand, so as to be easily depressed or pushed.  </h4>
@@ -72,7 +67,7 @@ const Attendance = (props) => {
                             <div className="p-4 ml-2 mr-2 mb-3 border-2 rounded-lg">
                                 <div className="border-2 rounded-lg p-3 bg-gray-800 md:h-100 relative">
                                     <div className="block relative h-48 rounded overflow-hidden">
-                                        <img alt="i2c_lcd" className="object-center w-full h-full block p-2 rounded-lg" src={i2c_lcd} />
+                                        <img alt="i2c_lcd" className="object-center w-full h-full block p-2 rounded-lg" src={require('../Images/ATTENDANCE/i2c_lcd.jpg').default} />
                                     </div>
                                     <h3 className="text-white text-2xl font-semibold mb-1 mt-3">I2C 16x2 LCD display</h3>
                                     <h4 className="text-white text-lg font-medium pb-4">It is able to display 16x2 characters on 2 lines, white characters on blue background. This I2C 16x2 Arduino LCD Screen is using an I2C communication interface. It means it only needs 4 pins for the LCD display: VCC, GND, SDA, SCL</h4>
@@ -85,7 +80,7 @@ const Attendance = (props) => {
                             <div className="p-4 ml-2 mr-2 mb-3 border-2 rounded-lg">
                                 <div className="border-2 rounded-lg p-3 bg-gray-800 md:h-100 relative">
                                     <div className="block relative h-48 rounded overflow-hidden">
-                                        <img alt="jumpers" className="object-center w-full h-full block p-2 rounded-lg" src={jumpers} />
+                                        <img alt="jumpers" className="object-center w-full h-full block p-2 rounded-lg" src={require('../Images/ATTENDANCE/jumpers.jpg').default} />
                                     </div>
                                     <h3 className="text-white text-2xl font-semibold mb-1 mt-3">Jumper Cables</h3>
                                     <h4 className="text-white text-lg font-medium pb-4">Jumper wires are used for making connections between items on your breadboard and your Arduino’s header pins. Use them to wire up all your circuits!</h4>
@@ -98,7 +93,7 @@ const Attendance = (props) => {
                             <div className="p-4 ml-2 mr-2 mb-3 border-2 rounded-lg">
                                 <div className="border-2 rounded-lg p-3 bg-gray-800 md:h-100 relative">
                                     <div className="block relative h-48 rounded overflow-hidden">
-                                        <img alt="breadboard" className="object-center w-full h-full block p-2 rounded-lg " src={breadboard} />
+                                        <img alt="breadboard" className="object-center w-full h-full block p-2 rounded-lg " src={require('../Images/ATTENDANCE/breadboard.jpg').default} />
                                     </div>
                                     <h3 className="text-white text-2xl font-semibold mb-1 mt-3">Bread Board</h3>
                                     <h4 className="text-white text-lg font-medium pb-4">A breadboard is used to build and test circuits quickly before finalizing any circuit design. The breadboard has many holes into which circuit components like ICs and resistors can be inserted.</h4>
@@ -265,84 +260,7 @@ const Attendance = (props) => {
                 </div>
             </div>
             <hr />
-            <div className="text-white text-4xl text-center mt-4">
-                Contributors to this project
-            </div>
-            <section className="text-white body-font">
-                <div className="px-5 py-12 mx-auto">
-                    <div className="flex flex-wrap -m-2 justify-center md:h-96">
-                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4  bg-gray-500 rounded-lg mr-2 ml-1 relative">
-                            <div className="h-full text-center">
-                                <img alt="Aakash" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={require('../Images/CONTRIBUTORS/Aakash.jpg').default}/>
-                                    <p className="text-3xl">Aakash Ghole</p>
-                                    <p className="leading-relaxed">3rd Year Computer Science Student at Atharva College of Engineering, Mumbai Unversity</p>
-                                    
-                                    <p className="text-white mb-3 mt-3">Full stack Web Developer,GUI Developer and Android Developer</p>
-                                    <div className="flex justify-center md:absolute md:bottom-1 md:inset-x-0">
-                                    <a href="https://github.com/aakashpy" target="_blank" rel="noreferrer"className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
-                                        src={require('../Images/github.png').default} alt="github"/>
-                                    </a>
-                                    <a href="https://www.instagram.com/aakashg_03/" target="_blank" rel="noreferrer"className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
-                                        src={require('../Images/insta.png').default} alt="insta"/>
-                                    </a>
-                                    <a href="https://www.linkedin.com/in/aakash-ghole-350332218/" target="_blank" rel="noreferrer"className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
-                                        src={require('../Images/linkdin.png').default} alt="linkdin"/>
-                                    </a>
-                                    </div>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4 bg-gray-500 rounded-lg mr-1 ml-1 relative">
-                            <div className="h-full text-center">
-                                <img alt="Sneha" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={require('../Images/CONTRIBUTORS/Sneha.jpg').default}/>
-                                <p className="text-3xl">Sneha Dhumal</p>
-                                    <p className="leading-relaxed">3rd Year Computer Science Student at Atharva College of Engineering, Mumbai Unversity</p>
-                                    
-                                    <p className="text-white mb-3 mt-3">Full stack Web Developer and UI designer</p>
-                                    <div className="flex justify-center md:absolute md:bottom-1 md:inset-x-0">
-                                    <a href="https://github.com/SnehaDhumal1508" target="_blank" rel="noreferrer"className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
-                                        src={require('../Images/github.png').default} alt="github"/>
-                                    </a>
-                                    <a href="https://www.instagram.com/snehadhumal_15/" target="_blank" rel="noreferrer"className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
-                                        src={require('../Images/insta.png').default} alt="insta"/>
-                                    </a>
-                                    <a href="https://www.linkedin.com/in/sneha-dhumal/" target="_blank" rel="noreferrer"className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
-                                        src={require('../Images/linkdin.png').default} alt="linkdin"/>
-                                    </a>
-                                    </div>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4 bg-gray-500 rounded-lg mr-1 ml-2 relative">
-                            <div className="h-full text-center">
-                                <img alt="Dhairya" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={require('../Images/CONTRIBUTORS/Dhairya.jpg').default}/>
-                                <p className="text-3xl">Dhairya Honrao</p>
-                                    <p className="leading-relaxed">3rd Year Computer Science Student at Atharva College of Engineering, Mumbai Unversity</p>           
-                                    <p className="text-white mb-3 mt-3">IOT specialist and UI designer</p>
-                                    <div className="flex justify-center md:absolute md:bottom-1 md:inset-x-0">
-                                    <a href="https://github.com/DhairyaHonrao" target="_blank" rel="noreferrer"className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
-                                        src={require('../Images/github.png').default} alt="github"/>
-                                    </a>
-                                    <a href="https://www.instagram.com/dhairya_honrao/" target="_blank" rel="noreferrer"className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
-                                        src={require('../Images/insta.png').default} alt="insta"/>
-                                    </a>
-                                    {/* <a href="https://www.linkedin.com/in/sneha-dhumal/" target="_blank" rel="noreferrer"className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
-                                        src={require('../Images/linkdin.png').default} alt="linkdin"/>
-                                    </a> */}
-                                    </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            
+            {<Contributors/>}   
         </div>
     )
 }
--- client/src/Components/Projects_List/Shot.js
+++ client/src/Components/Projects_List/Shot.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
+import Contributors from '../../Static_Component/Contributors'
+
 const Shot = (props) => {
     return (
         <div className="bg-gradient-to-r from-blue-900 via-black to-blue-900 h-auto text-white">
@@ -135,83 +137,7 @@ const Shot = (props) => {
                 </Splide>
             </div>
             <hr />
-            <div className="text-white text-4xl text-center mt-4">
-                Contributors to this project
-            </div>
-            <section className="text-white body-font">
-                <div className="px-5 py-12 mx-auto">
-                    <div className="flex flex-wrap -m-2 justify-center md:h-96">
-                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4  bg-gray-500 rounded-lg mr-2 ml-1 relative">
-                            <div className="h-full text-center">
-                                <img alt="Aakash" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={require('../Images/CONTRIBUTORS/Aakash.jpg').default} />
-                                <p className="text-3xl">Aakash Ghole</p>
-                                <p className="leading-relaxed">3rd Year Computer Science Student at Atharva College of Engineering, Mumbai Unversity</p>
-
-                                <p className="text-white mb-3 mt-3">Full stack Web Developer,GUI Developer and Android Developer</p>
-                                <div className="flex justify-center md:absolute md:bottom-1 md:inset-x-0">
-                                    <a href="https://github.com/aakashpy" target="_blank" rel="noreferrer" className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
-                                            src={require('../Images/github.png').default} alt="github" />
-                                    </a>
-                                    <a href="https://www.instagram.com/aakashg_03/" target="_blank" rel="noreferrer" className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
-                                            src={require('../Images/insta.png').default} alt="insta" />
-                                    </a>
-                                    <a href="https://www.linkedin.com/in/aakash-ghole-350332218/" target="_blank" rel="noreferrer" className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
-                                            src={require('../Images/linkdin.png').default} alt="linkdin" />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4 bg-gray-500 rounded-lg mr-1 ml-1 relative">
-                            <div className="h-full text-center">
-                                <img alt="Sneha" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={require('../Images/CONTRIBUTORS/Sneha.jpg').default} />
-                                <p className="text-3xl">Sneha Dhumal</p>
-                                <p className="leading-relaxed">3rd Year Computer Science Student at Atharva College of Engineering, Mumbai Unversity</p>
-
-                                <p className="text-white mb-3 mt-3">Full stack Web Developer and UI designer</p>
-                                <div className="flex justify-center md:absolute md:bottom-1 md:inset-x-0">
-                                    <a href="https://github.com/SnehaDhumal1508" target="_blank" rel="noreferrer" className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
-                                            src={require('../Images/github.png').default} alt="github" />
-                                    </a>
-                                    <a href="https://www.instagram.com/snehadhumal_15/" target="_blank" rel="noreferrer" className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
-                                            src={require('../Images/insta.png').default} alt="insta" />
-                                    </a>
-                                    <a href="https://www.linkedin.com/in/sneha-dhumal/" target="_blank" rel="noreferrer" className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
-                                            src={require('../Images/linkdin.png').default} alt="linkdin" />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 lg:mb-0 mb-6 p-4 bg-gray-500 rounded-lg mr-1 ml-2 relative">
-                            <div className="h-full text-center">
-                                <img alt="Dhairya" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={require('../Images/CONTRIBUTORS/Dhairya.jpg').default} />
-                                <p className="text-3xl">Dhairya Honrao</p>
-                                <p className="leading-relaxed">3rd Year Computer Science Student at Atharva College of Engineering, Mumbai Unversity</p>
-                                <p className="text-white mb-3 mt-3">IOT specialist and UI designer</p>
-                                <div className="flex justify-center md:absolute md:bottom-1 md:inset-x-0">
-                                    <a href="https://github.com/DhairyaHonrao" target="_blank" rel="noreferrer" className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
-                                            src={require('../Images/github.png').default} alt="github" />
-                                    </a>
-                                    <a href="https://www.instagram.com/dhairya_honrao/" target="_blank" rel="noreferrer" className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
-                                            src={require('../Images/insta.png').default} alt="insta" />
-                                    </a>
-                                    {/* <a href="https://www.linkedin.com/in/sneha-dhumal/" target="_blank" rel="noreferrer"className="text-white">
-                                        <img className="w-12 h-12 mb-8 ml-2 mr-2 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" 
-                                        src={require('../Images/linkdin.png').default} alt="linkdin"/>
-                                    </a> */}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            {<Contributors/>}
         </div>
     )
 }
